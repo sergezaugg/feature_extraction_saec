39894fa953fcb419b4e1be421b1dfb3bc84962de
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-24 16:10:27 +0200

    misc

--- dev_scripts/build.sh
+++ dev_scripts/build.sh
@@ -1,19 +1,3 @@
 python -m venv ./
-
 python -m pip install --upgrade build
-
 python -m build
-
-
-
-pip uninstall fe_saec 
-
-python -m venv ./
-
-pip install dist/fe_saec-0.0.1-py3-none-any.whl
-
-pip install --force-reinstall torch torchvision --index-url https://download.pytorch.org/whl/cu126
-
-
-
-
---
+++ dev_scripts/pipinstall.sh
@@ -0,0 +1,11 @@
+python -m venv ./
+
+pip uninstall fe_saec 
+
+pip install dist/fe_saec-0.0.1-py3-none-any.whl
+
+pip install --force-reinstall torch torchvision --index-url https://download.pytorch.org/whl/cu126
+
+
+
+
