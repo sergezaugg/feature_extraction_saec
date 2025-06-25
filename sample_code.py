4c3394fc11aa83b5a0676398f86c3d25925eadc2
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-25 15:58:18 +0200

    adjusted sample_code.py to new arguments

--- sample_code.py
+++ sample_code.py
@@ -9,15 +9,20 @@ from fe_saec import SAEC_extractor
 torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# Initialize a AEC-extractor instance
-path_models = "D:/xc_real_projects/pytorch_hot_models_keep"
-model_tag = "20250617_150956"
+# define full path to a TorchScript model
+path_model = "D:/xc_real_projects/pytorch_hot_models_keep/20250617_150956_encoder_script_GenC_new_TP32_epo007.pth"
+# path to dir with images 
 path_images  = "D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
-ae = SAEC_extractor(path_models, model_tag, path_images, device = device)
-
+# instantiate with a model
+ae = SAEC_extractor(path_model = path_model, device = device) 
 # extract (will save to disk as npz)
-ae.extract(devel = True)
+ae.extract(image_path = path_images, batch_size = 16, shuffle = True , devel = True) 
+# Check dim of internally stores array 
+ae.X.shape
+ae.N.shape
 # time pool
-ae.time_pool()
+ae.time_pool(ecut=0)
+ae.time_pool(ecut=2)
 # dim reduce
-[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
+[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8]]  
+
