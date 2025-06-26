0cb3af08e5f8a66e08330925149fbcbeb08596bb
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-26 09:56:35 +0200

    build v0.9.6 tested ok

--- dev/dev_scripts/main_dev.py
+++ dev/dev_scripts/main_dev.py
@@ -19,12 +19,12 @@ path_save = "./dev/dev_outp"
 
 ae = SAEC_extractor(path_model = path_model, device = device) 
 # extract (will save to disk as npz)
-ae.extract(image_path = path_images, fe_save_path = path_save, batch_size = 16, shuffle = True , devel = True) # , ecut = 1)
+ae.extract(image_path = path_images, fe_save_path = path_save, batch_size = 16, shuffle = True , devel = True) 
 # time pool
 ae.time_pool(ecut=2)
 ae.time_pool()
 # dim reduce
-ae.reduce_dimension(n_neigh = 10, reduced_dim = 11) 
+ae.reduce_dimension(n_neigh = 10, reduced_dim = 10) 
 
 ae.time_stamp_model
 ae.X.shape
--- dev/dev_scripts/pipinstall.sh
+++ dev/dev_scripts/pipinstall.sh
@@ -2,7 +2,7 @@ python -m venv ./
 
 pip uninstall fe_saec 
 
-pip install dist/fe_saec-0.9.5.dev0-py3-none-any.whl
+pip install dist/fe_saec-0.9.6-py3-none-any.whl
 pip install https://github.com/sergezaugg/feature_extraction_saec/releases/download/v0.9.3/fe_saec-0.9.3-py3-none-any.whl
 
 pip install --force-reinstall torch torchvision --index-url https://download.pytorch.org/whl/cu126
